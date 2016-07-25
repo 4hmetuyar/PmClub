c1d56fea1d8e660d8e340f393f1cc4aa68256aad
sahramert <sahra__mert@hotmail.com>
2016-07-25 21:57:00 +0300

    Kodlar düzeltildi.

---
+++ PmClup/Controllers/FairsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PmClup.Controllers
+{
+    public class FairsController : Controller
+    {
+        // GET: Fairs
+        public ActionResult Modef()
+        {
+            return View();
+        }
+        public ActionResult Imob()
+        {
+            return View();
+        }
+        public ActionResult ModefExpo()
+        {
+            return View();
+        }
+        public ActionResult Modef2015()
+        {
+            return View();
+        }
+    }
+}
---
+++ PmClup/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PmClup.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult CornerSofa()
+        {
+            return View();
+        }
+        public ActionResult Puffs()
+        {
+            return View();
+        }
+        public ActionResult ArmchairFor()
+        {
+            return View();
+        }
+        public ActionResult SofaSuit()
+        {
+            return View();
+        }
+
+
+    }
+}
